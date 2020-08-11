a621a7483c191bd4422b333663cfa69274f8c920
MalihaKabir <malihakabir01@gmail.com>
2020-08-11 13:53:33 +0600

    updated rule, match four not six

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# drop-six-game
+# drop-four-game
 A retro grid-based game in vanilla JavaScript, HTML and CSS
 
-Drop Six is a two-player board game in which the players first choose a color and then take turns dropping one colored disc from the top into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a vertical line of six of one's own discs.
+Drop Four is a two-player board game in which the players first choose a color and then take turns dropping one colored disc from the top into a seven-column, four-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a vertical line of four of one's own discs.
 
-## Rules of Connect Six
+## Rules of Connect Four
 * You will start as player One
 * You will take your go as player One
 * You will not be allowed to go in squares that are not at the bottom of the grid, or do not have a taken square under it
 * Once your go is taken and you did not win, it'll be player Two's go
-* The gamer will continue until a winning combination of six is reached by player One or player Two, or until the grid is full and no winning combination was found
+* The gamer will continue until a winning combination of four is reached by player One or player Two, or until the grid is full and no winning combination was found
 * The first place to do this, wins.
--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const currentPlayerDisplay = document.querySelector('.current-player');
-	// const scoreOneDisplay = document.querySelector('.score-one');
-	// const scoreTwoDisplay = document.querySelector('.score-two');
 	const winningPlayer = document.querySelector('#winning-player');
-	// const startBtn = document.querySelector('#start-btn');
 	const smallGrids = document.querySelectorAll('.grid div');
 	let currentPlayer = 1;
-	// let scoreOne = 0;
-	// let scoreTwo = 0;
 
 	for (let i = 0; i < smallGrids.length; i++) {
 		smallGrids[i].addEventListener('click', () => {
@@ -20,25 +15,15 @@ document.addEventListener('DOMContentLoaded', () => {
 					smallGrids[i].classList.add('ball-one');
 					currentPlayer = 2;
 					currentPlayerDisplay.textContent = currentPlayer;
-
-					// smallGrids[i].id = i;
-
 					currentPlayerDisplay.classList.remove('player-one');
 					currentPlayerDisplay.classList.add('player-two');
 				} else if (currentPlayer === 2) {
 					smallGrids[i].classList.add('ball-two');
 					currentPlayer = 1;
 					currentPlayerDisplay.textContent = currentPlayer;
-
-					// smallGrids[i].id = i;
-
 					currentPlayerDisplay.classList.remove('player-two');
 					currentPlayerDisplay.classList.add('player-one');
 				}
-				// console.log('FOR', smallGrids[i], currentPlayerDisplay, i);
-				// console.log('findClassName', smallGrids[i].className);
-
-				// console.log(smallGrids[i]);
 			} else {
 				alert('No gap between squares is allowed');
 			}
@@ -47,13 +32,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	function getScore () {
 		const winningArrays = [
-			[ 35, 28, 21, 14, 7, 0 ],
-			[ 36, 29, 22, 15, 8, 1 ],
-			[ 37, 30, 23, 16, 9, 2 ],
-			[ 38, 31, 24, 17, 10, 3 ],
-			[ 39, 32, 25, 18, 11, 4 ],
-			[ 40, 33, 26, 19, 12, 5 ],
-			[ 41, 34, 27, 20, 13, 6 ],
+			[ 35, 28, 21, 14 ],
+			[ 28, 21, 14, 7 ],
+			[ 21, 14, 7, 0 ],
+			[ 36, 29, 22, 15 ],
+			[ 29, 22, 15, 8 ],
+			[ 22, 15, 8, 1 ],
+			[ 37, 30, 23, 162 ],
+			[ 30, 23, 16, 9 ],
+			[ 23, 16, 9, 2 ],
+			[ 38, 31, 24, 17 ],
+			[ 31, 24, 17, 10 ],
+			[ 24, 17, 10, 3 ],
+			[ 39, 32, 25, 18 ],
+			[ 32, 25, 18, 11 ],
+			[ 25, 18, 11, 4 ],
+			[ 40, 33, 26, 19 ],
+			[ 33, 26, 19, 12 ],
+			[ 26, 19, 12, 5 ],
+			[ 41, 34, 27, 20 ],
+			[ 34, 27, 20, 13 ],
+			[ 27, 20, 13, 6 ],
 		];
 
 		winningArrays.map((smallArray) => {
@@ -61,43 +60,27 @@ document.addEventListener('DOMContentLoaded', () => {
 			const smallGridTwo = smallGrids[smallArray[1]];
 			const smallGridThree = smallGrids[smallArray[2]];
 			const smallGridFour = smallGrids[smallArray[3]];
-			const smallGridFive = smallGrids[smallArray[4]];
-			const smallGridSix = smallGrids[smallArray[5]];
-
 			if (
 				smallGridOne.classList.contains('ball-one') &&
 				smallGridTwo.classList.contains('ball-one') &&
 				smallGridThree.classList.contains('ball-one') &&
-				smallGridFour.classList.contains('ball-one') &&
-				smallGridFive.classList.contains('ball-one') &&
-				smallGridSix.classList.contains('ball-one')
+				smallGridFour.classList.contains('ball-one')
 			) {
-				// scoreOne += 10;
-				// scoreOneDisplay.textContent = scoreOne;
-				// currentPlayer = 1;
 				winningPlayer.textContent = `Player ${currentPlayer - 1}`;
 				winningPlayer.classList.add('player-one');
 				smallGrids.forEach((smallGrid) => smallGrid.removeEventListener('click', getScore));
-				// startBtn.removeEventListener('click', startGame);
 			} else if (
 				smallGridOne.className === 'ball-two' &&
 				smallGridTwo.className === 'ball-two' &&
 				smallGridThree.className === 'ball-two' &&
-				smallGridFour.className === 'ball-two' &&
-				smallGridFive.className === 'ball-two' &&
-				smallGridSix.className === 'ball-two'
+				smallGridFour.className === 'ball-two'
 			) {
-				// scoreTwo += 10;
-				// scoreTwoDisplay.textContent = scoreTwo;
-				// currentPlayer = 2;
 				winningPlayer.textContent = `Player ${currentPlayer + 1}`;
 				winningPlayer.classList.add('player-two');
 				smallGrids.forEach((smallGrid) => smallGrid.removeEventListener('click', getScore));
-				// startBtn.removeEventListener('click', startGame);
 			}
 		});
 	}
 
 	smallGrids.forEach((smallGrid) => smallGrid.addEventListener('click', getScore));
-	// startBtn.addEventListener('click', startGame);
 });
