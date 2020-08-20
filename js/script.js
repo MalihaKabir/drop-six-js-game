19786473ca8de3ee9179bfe2b135532538cadb84
MalihaKabir <malihakabir01@gmail.com>
2020-08-21 04:52:16 +0600

    added start button

--- js/script.js
+++ js/script.js
@@ -1,117 +1,163 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const currentPlayerDisplay = document.querySelector('.current-player');
-	const gameOver = document.getElementById('game-over');
 	const winningPlayer = document.querySelector('#winning-player');
+	const startBtn = document.getElementById('start-btn');
 	const smallGrids = document.querySelectorAll('.grid div');
-	let currentPlayer = 1;
+	let currentPlayerOne = 1;
+	let currentPlayerTwo = 2;
+	let currentPlayer = currentPlayerOne;
 
-	for (let i = 0; i < smallGrids.length; i++) {
-		smallGrids[i].addEventListener('click', () => {
-			if (
-				smallGrids[i + 7].className === 'taken' ||
-				smallGrids[i + 7].className === 'ball-one' ||
-				smallGrids[i + 7].className === 'ball-two'
-			) {
-				if (currentPlayer === 1) {
-					smallGrids[i].classList.add('ball-one');
-					currentPlayer = 2;
-					currentPlayerDisplay.textContent = currentPlayer;
-					currentPlayerDisplay.classList.remove('player-one');
-					currentPlayerDisplay.classList.add('player-two');
-				} else if (currentPlayer === 2) {
-					smallGrids[i].classList.add('ball-two');
-					currentPlayer = 1;
-					currentPlayerDisplay.textContent = currentPlayer;
-					currentPlayerDisplay.classList.remove('player-two');
-					currentPlayerDisplay.classList.add('player-one');
-				}
-			} else {
-				alert('No gap between squares is allowed');
-			}
+	function startGame () {
+		currentPlayer = currentPlayerOne;
+		currentPlayerDisplay.textContent = currentPlayer;
+		winningPlayer.textContent = '. . .';
+
+		currentPlayerDisplay.classList.add('player-one');
+		currentPlayerDisplay.classList.remove('player-two');
+		smallGrids.forEach((smallGrid) => smallGrid.classList.remove('ball-one', 'ball-two'));
+		winningPlayer.classList.remove('player-one', 'player-two');
+
+		makeGridUnclickable();
+		makeGridClickable();
+		smallGrids.forEach((smallGrid) => smallGrid.removeEventListener('click', getScore));
+		smallGrids.forEach((smallGrid) => smallGrid.addEventListener('click', getScore));
+	}
+
+	function makeGridUnclickable () {
+		smallGrids.forEach((smallGrid, i) => {
+			smallGrid.removeEventListener('click', () => {
+				clickableSquares(i);
+			});
 		});
+	}
+	function makeGridClickable () {
+		smallGrids.forEach((smallGrid, i) => {
+			smallGrid.addEventListener('click', () => {
+				clickableSquares(i);
+			});
+		});
+	}
+
+	function clickableSquares (gridIndex) {
+		if (
+			smallGrids[gridIndex + 7].className === 'taken' ||
+			smallGrids[gridIndex + 7].className === 'ball-one' ||
+			smallGrids[gridIndex + 7].className === 'ball-two'
+		) {
+			if (currentPlayer === currentPlayerOne) {
+				smallGrids[gridIndex].classList.remove('ball-two');
+				smallGrids[gridIndex].classList.add('ball-one');
+				currentPlayer = currentPlayerTwo;
+				currentPlayerDisplay.textContent = currentPlayer;
+
+				smallGrids[gridIndex].id = gridIndex;
+
+				currentPlayerDisplay.classList.remove('player-one');
+				currentPlayerDisplay.classList.add('player-two');
+			} else if (currentPlayer === currentPlayerTwo) {
+				smallGrids[gridIndex].classList.remove('ball-one');
+				smallGrids[gridIndex].classList.add('ball-two');
+				currentPlayer = currentPlayerOne;
+				currentPlayerDisplay.textContent = currentPlayer;
+
+				smallGrids[gridIndex].id = gridIndex;
+
+				currentPlayerDisplay.classList.remove('player-two');
+				currentPlayerDisplay.classList.add('player-one');
+			}
+		} else {
+			alert('No gap between squares is allowed');
+		}
 	}
 
 	function getScore () {
 		const winningArrays = [
 			// vertically
-			[35, 28, 21, 14],
-			[28, 21, 14, 7],
-			[21, 14, 7, 0],
-			[36, 29, 22, 15],
-			[29, 22, 15, 8],
-			[22, 15, 8, 1],
-			[37, 30, 23, 16],
-			[30, 23, 16, 9],
-			[23, 16, 9, 2],
-			[38, 31, 24, 17],
-			[31, 24, 17, 10],
-			[24, 17, 10, 3],
-			[39, 32, 25, 18],
-			[32, 25, 18, 11],
-			[25, 18, 11, 4],
-			[40, 33, 26, 19],
-			[33, 26, 19, 12],
-			[26, 19, 12, 5],
-			[41, 34, 27, 20],
-			[34, 27, 20, 13],
-			[27, 20, 13, 6],
+			[ 35, 28, 21, 14 ],
+			[ 28, 21, 14, 7 ],
+			[ 21, 14, 7, 0 ],
+			[ 36, 29, 22, 15 ],
+			[ 29, 22, 15, 8 ],
+			[ 22, 15, 8, 1 ],
+			[ 37, 30, 23, 16 ],
+			[ 30, 23, 16, 9 ],
+			[ 23, 16, 9, 2 ],
+			[ 38, 31, 24, 17 ],
+			[ 31, 24, 17, 10 ],
+			[ 24, 17, 10, 3 ],
+			[ 39, 32, 25, 18 ],
+			[ 32, 25, 18, 11 ],
+			[ 25, 18, 11, 4 ],
+			[ 40, 33, 26, 19 ],
+			[ 33, 26, 19, 12 ],
+			[ 26, 19, 12, 5 ],
+			[ 41, 34, 27, 20 ],
+			[ 34, 27, 20, 13 ],
+			[ 27, 20, 13, 6 ],
 			// horizontally
-			[35, 36, 37, 38],
-			[36, 37, 38, 39],
-			[37, 38, 39, 40],
-			[38, 39, 40, 41],
-			[28, 29, 30, 31],
-			[29, 30, 31, 32],
-			[30, 31, 32, 33],
-			[31, 32, 33, 34],
-			[21, 22, 23, 24],
-			[22, 23, 24, 25],
-			[23, 24, 25, 26],
-			[24, 25, 26, 27],
-			[14, 15, 16, 17],
-			[15, 16, 17, 18],
-			[16, 17, 18, 19],
-			[17, 18, 19, 20],
-			[7, 8, 9, 10],
-			[8, 9, 10, 11],
-			[9, 10, 11, 12],
-			[10, 11, 12, 13],
-			[0, 1, 2, 3],
-			[1, 2, 3, 4],
-			[2, 3, 4, 5],
-			[3, 4, 5, 6],
+			[ 35, 36, 37, 38 ],
+			[ 36, 37, 38, 39 ],
+			[ 37, 38, 39, 40 ],
+			[ 38, 39, 40, 41 ],
+			[ 28, 29, 30, 31 ],
+			[ 29, 30, 31, 32 ],
+			[ 30, 31, 32, 33 ],
+			[ 31, 32, 33, 34 ],
+			[ 21, 22, 23, 24 ],
+			[ 22, 23, 24, 25 ],
+			[ 23, 24, 25, 26 ],
+			[ 24, 25, 26, 27 ],
+			[ 14, 15, 16, 17 ],
+			[ 15, 16, 17, 18 ],
+			[ 16, 17, 18, 19 ],
+			[ 17, 18, 19, 20 ],
+			[ 7, 8, 9, 10 ],
+			[ 8, 9, 10, 11 ],
+			[ 9, 10, 11, 12 ],
+			[ 10, 11, 12, 13 ],
+			[ 0, 1, 2, 3 ],
+			[ 1, 2, 3, 4 ],
+			[ 2, 3, 4, 5 ],
+			[ 3, 4, 5, 6 ],
 		];
 
-		winningArrays.map((smallArray) => {
+		winningArrays.forEach((smallArray) => {
 			const smallGridOne = smallGrids[smallArray[0]];
 			const smallGridTwo = smallGrids[smallArray[1]];
 			const smallGridThree = smallGrids[smallArray[2]];
 			const smallGridFour = smallGrids[smallArray[3]];
+
 			if (
 				smallGridOne.classList.contains('ball-one') &&
 				smallGridTwo.classList.contains('ball-one') &&
 				smallGridThree.classList.contains('ball-one') &&
 				smallGridFour.classList.contains('ball-one')
 			) {
-				winningPlayer.textContent = `Player ${currentPlayer - 1}`;
+				winningPlayer.textContent = `Player ${currentPlayer - 1}! Game over.`;
 				winningPlayer.classList.add('player-one');
-				gameOver.textContent = 'GAME OVER!';
-				gameOver.classList.add('player-one');
+
+				currentPlayerDisplay.classList.add('player-one');
+
+				makeGridUnclickable();
 				smallGrids.forEach((smallGrid) => smallGrid.removeEventListener('click', getScore));
+				startBtn.removeEventListener('click', startGame);
+				startBtn.addEventListener('click', startGame);
 			} else if (
 				smallGridOne.className === 'ball-two' &&
 				smallGridTwo.className === 'ball-two' &&
 				smallGridThree.className === 'ball-two' &&
 				smallGridFour.className === 'ball-two'
 			) {
-				winningPlayer.textContent = `Player ${currentPlayer + 1}`;
+				winningPlayer.textContent = `Player ${currentPlayer + 1}! Game over.`;
 				winningPlayer.classList.add('player-two');
-				gameOver.textContent = 'GAME OVER!';
-				gameOver.classList.add('player-two');
+
+				makeGridUnclickable();
 				smallGrids.forEach((smallGrid) => smallGrid.removeEventListener('click', getScore));
+				startBtn.removeEventListener('click', startGame);
+				startBtn.addEventListener('click', startGame);
 			}
 		});
 	}
 
-	smallGrids.forEach((smallGrid) => smallGrid.addEventListener('click', getScore));
+	startBtn.addEventListener('click', startGame);
 });
